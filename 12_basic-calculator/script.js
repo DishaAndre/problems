e0239191f15dfb5edbe12e3a6c7d9e421f2dfa2f
Disha B. Andre <andredisha55@gmail.com>
2026-04-25 22:14:46 +0530

    Implement basic arithmetic functions in script.js

---
+++ 12_basic-calculator/script.js
@@ -0,0 +1,28 @@
+function add() {
+    let a = Number(document.getElementById("num1").value);
+    let b = Number(document.getElementById("num2").value);
+    document.getElementById("result").innerText = "Result: " + (a + b);
+}
+
+function sub() {
+    let a = Number(document.getElementById("num1").value);
+    let b = Number(document.getElementById("num2").value);
+    document.getElementById("result").innerText = "Result: " + (a - b);
+}
+
+function mul() {
+    let a = Number(document.getElementById("num1").value);
+    let b = Number(document.getElementById("num2").value);
+    document.getElementById("result").innerText = "Result: " + (a * b);
+}
+
+function div() {
+    let a = Number(document.getElementById("num1").value);
+    let b = Number(document.getElementById("num2").value);
+
+    if (b === 0) {
+        document.getElementById("result").innerText = "Cannot divide by zero";
+    } else {
+        document.getElementById("result").innerText = "Result: " + (a / b);
+    }
+}
